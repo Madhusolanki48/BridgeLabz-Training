fc2a1eac380744afc50a269b1ca68daea3db4d67
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-12-29 20:22:44 +0530

    [Madhu Solanki] feat: add UC-2 daily wage calculation

--- core-java-practice/scenario-based/EmployeeWageComputation.java
+++ core-java-practice/scenario-based/EmployeeWageComputation.java
@@ -1,12 +1,18 @@
 package scenario_based;
 import java.util.Random;
 public class EmployeeWageComputation {
+    static final int wagePerHour = 20;  //assume wage per hour =20
+    static final int fullDayWorkingHours = 8;  //full day hour =8
     public static void main(String[] args) {
     	System.out.println("Welcome to Employee Wage Computation Program on Master Branch\n");
-    }  
+    // Use Case -2 : Compute Daily Employee Wage
+            int dailyWage = wagePerHour * fullDayWorkingHours;
+            System.out.println("Daily Employee Wage = ₹" + dailyWage + "\n");
+    }
     // Use Case-1 : Check if employee is present or absent
     public static boolean isEmployeePresent() {
         Random random = new Random();
         return random.nextInt(2) == 1;
     }
+    
 }
