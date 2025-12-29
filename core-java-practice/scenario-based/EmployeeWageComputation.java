3663e4eefe8d9f6d3c815421a179a6e6521282d2
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-12-29 20:26:22 +0530

    [Madhu Solanki] feat: add UC-3 part-time employee wage calculation

--- core-java-practice/scenario-based/EmployeeWageComputation.java
+++ core-java-practice/scenario-based/EmployeeWageComputation.java
@@ -2,7 +2,8 @@ package scenario_based;
 import java.util.Random;
 public class EmployeeWageComputation {
     static final int wagePerHour = 20;  //assume wage per hour =20
-    static final int fullDayWorkingHours = 8;  //full day hour =8
+    static final int fullDayWorkingHours = 8;  //full day hours =8
+    static final int partTimeWorkingHours = 4; //part-time hours =4
     public static void main(String[] args) {
     	System.out.println("Welcome to Employee Wage Computation Program on Master Branch\n");
     // Use Case -2 : Compute Daily Employee Wage
@@ -13,6 +14,10 @@ public class EmployeeWageComputation {
     public static boolean isEmployeePresent() {
         Random random = new Random();
         return random.nextInt(2) == 1;
+    }
+    // Use Case -3 : Add Part time Employee & Wage
+    public static int getEmployeeType() {
+        return random.nextInt(2) + 1; // 1 = Part Time, 2 = Full Time
     }
     
 }
