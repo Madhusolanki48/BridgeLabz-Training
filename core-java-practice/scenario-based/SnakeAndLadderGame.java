cf8ee61f6d274efa0d6d96b1efe3c19b9064d9db
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-12-30 15:30:13 +0530

    [madhu solanki] Refactor. Added UC-3 switch case for player options

--- core-java-practice/scenario-based/SnakeAndLadderGame.java
+++ core-java-practice/scenario-based/SnakeAndLadderGame.java
@@ -3,21 +3,43 @@ import java.util.*;
 public class SnakeAndLadderGame {
 	public static void main(String[] args) {
 		System.out.println("----------------- Let's Play Snake & Ladder Game -----------------");
-		
+		System.out.println("------------------------------------------------------------------");
 		// Use Case-1 : Single player at start position 0
 		
         int playerPosition = 0;   // start position
         
         System.out.println("Game starts with single player at position : " + playerPosition);
-
+        System.out.println("------------------------------------------------------------------");
         // Use Case-2 : The Player rolls the die to get a number between 1 to 6 using Random
 
         Random random = new Random();
         int diceValue = random.nextInt(6) + 1;
-        System.out.println("Player rolls the die");
+        System.out.println("Player rolled the die");
         System.out.println("Dice value : " + diceValue);
 
-        
+        // Use Case-3 : Check Option — No Play / Ladder / Snake
+
+        int option = random.nextInt(3);  // options: 0 = No Play, 1 = Ladder, 2 = Snake
+        switch (option) {
+            case 0:    // No play
+                System.out.println("Option selected : NO PLAY");
+                System.out.println("Player stays at the same position.");
+                break;
+            case 1:   // Ladder
+                System.out.println("Option selected : LADDER");
+                playerPosition += diceValue;
+                System.out.println("Player climbs the ladder! New position : " + playerPosition);
+                break;
+            case 2:   // Snake
+                System.out.println("Option selected : SNAKE");
+                playerPosition -= diceValue;
+                if (playerPosition < 0)
+                playerPosition = 0;
+                System.out.println("Player bitten by snake! New Position : " + playerPosition);
+                break;
+        }
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Current Player Position = " + playerPosition);
     }
 }
 	
