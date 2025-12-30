9634a47dbecfed18af4fae98dc2b58a71dfec0d4
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-12-30 15:11:44 +0530

    [madhu solanki] Refactor. Added UC-2 player dice roll functionality

--- core-java-practice/scenario-based/SnakeAndLadderGame.java
+++ core-java-practice/scenario-based/SnakeAndLadderGame.java
@@ -9,6 +9,15 @@ public class SnakeAndLadderGame {
         int playerPosition = 0;   // start position
         
         System.out.println("Game starts with single player at position : " + playerPosition);
+
+        // Use Case-2 : The Player rolls the die to get a number between 1 to 6 using Random
+
+        Random random = new Random();
+        int diceValue = random.nextInt(6) + 1;
+        System.out.println("Player rolls the die");
+        System.out.println("Dice value : " + diceValue);
+
+        
     }
 }
 	
