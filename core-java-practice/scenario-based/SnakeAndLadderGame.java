cd839025c115042b8c011fc5900b3f41fdc463d9
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-12-30 14:58:50 +0530

    [madhu solanki] Add. Added UC-1 Snake and Ladder game start position

---
+++ core-java-practice/scenario-based/SnakeAndLadderGame.java
@@ -0,0 +1,15 @@
+package scenario_based;
+import java.util.*;
+public class SnakeAndLadderGame {
+	public static void main(String[] args) {
+		System.out.println("----------------- Let's Play Snake & Ladder Game -----------------");
+		
+		// Use Case-1 : Single player at start position 0
+		
+        int playerPosition = 0;   // start position
+        
+        System.out.println("Game starts with single player at position : " + playerPosition);
+    }
+}
+	
+	    
