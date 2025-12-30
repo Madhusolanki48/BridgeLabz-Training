dc7745903fc4f6d2b1c162ea904a2c1c1cb25899
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-12-30 22:38:25 +0530

    [madhu solanki] Refactor. Added UC-7 two player Snake & Ladder Game with winner result

--- core-java-practice/scenario-based/SnakeAndLadderGame.java
+++ core-java-practice/scenario-based/SnakeAndLadderGame.java
@@ -5,27 +5,31 @@ public class SnakeAndLadderGame {
 		System.out.println("----------------- Let's Play Snake & Ladder Game -----------------");
 		System.out.println("------------------------------------------------------------------");
 		// Use Case-1 : Single player at start position 0
-		
-        int playerPosition = 0;   // start position
-        
-        System.out.println("Game starts with single player at position : " + playerPosition);
+        // Use Case-7 :Play the game with 2 Players
+        int player1Position = 0;
+        int player2Position = 0;
+        int currentPlayer = 1;   // 1 for Player1, 2 for Player2
+
+        // System.out.println("Game starts with single player at position : " + playerPosition);
+        System.out.println("Game starts with two players at position : 0");
         System.out.println("------------------------------------------------------------------");
         // Use Case-2 : The Player rolls the die to get a number between 1 to 6 using Random
 
         Random random = new Random();
-        int turn=0;           //-----for use case-6
+        int diceRollCount=0;           //-----for use case-6
         // Use Case-4 : Repeat till the Player reaches the winning position 100 
-        while (playerPosition < 100) {
-            turn+=1;
+        while (player1Position < 100 && player2Position < 100) {
+            diceRollCount+=1;
         int diceValue = random.nextInt(6) + 1;
-        System.out.println("Player rolled the die");
+        System.out.println("\nPlayer" + currentPlayer + " rolled the die");
         System.out.println("Dice value : " + diceValue);
         // Use Case-3 : Check Option — No Play / Ladder / Snake
         int option = random.nextInt(3);  // options: 0 = No Play, 1 = Ladder, 2 = Snake
+        int playerPosition = (currentPlayer == 1) ? player1Position : player2Position;
         switch (option) {
             case 0:    // No play
                 System.out.println("Option selected : NO PLAY");
-                System.out.println("Player stays at the same position.");
+                System.out.println("\nPlayer stays at the same position.");
                 break;
             case 1:   // Ladder
                 System.out.println("Option selected : LADDER");
@@ -35,7 +39,8 @@ public class SnakeAndLadderGame {
                     System.out.println("Move exceeds 100, so stay at the same position");
                     playerPosition-=diceValue;
                 }else{
-                System.out.println("Player climbs the ladder! New position : " + playerPosition);
+                System.out.println("\nLadder! Player" + currentPlayer + " climbs to position " + playerPosition+" & gets another turn!");
+     
                 }
                 break;
             case 2:   // Snake
@@ -43,14 +48,33 @@ public class SnakeAndLadderGame {
                 playerPosition -= diceValue;
                 if (playerPosition < 0)
                 playerPosition = 0;
-                System.out.println("Player bitten by snake! New Position : " + playerPosition);
+                System.out.println("\nPlayer bitten by snake! New Position : " + playerPosition);
                 break;
-        }
-        System.out.println("------------------------------------------------------------------");
-        System.out.println("Current Player Position = " + playerPosition);   //the position after every die role
+            } 
+            //update position
+            if (currentPlayer == 1)
+            player1Position = playerPosition;
+            else
+            player2Position = playerPosition;
+            System.out.println("------------------------------------------------------------------");
+            System.out.println("Player1 Current Position = " + player1Position);
+            System.out.println("Player2 Current Position = " + player2Position);
+
+            //switch turn only if it was not  a ladder
+            if (option != 1) {
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
+                System.out.println("\nSwitching turn to Player" + currentPlayer);
+            }
     }
         // Use Case-6 : Report the number of times the dice was played to win the game   
-        System.out.println("\nNumber of times the dice was played to win the game = " + turn);
-        System.out.println("\nHurray ! Player reached WINNING POSITION 100");
+        // System.out.println("\nNumber of times the dice was played to win the game = " + turn);
+        // System.out.println("\nHurray ! Player reached WINNING POSITION 100");
+
+        System.out.println("\n------------------------    Game Over    -------------------------");
+        System.out.println("+++++++++++++ Congratulations!! Player" + (player1Position == 100 ? 1 : 2) + " won the Game +++++++++++++");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Total Dice Rolls = " + diceRollCount);
+        System.out.println("-----------------------  Thanks for playing!  --------------------");
+
     }
 }
