ce752859710c98e8ca7623f21b71bb0fca833b07
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2026-01-12 23:01:19 +0530

    [madhu solanki] Add .Added Student Course Registration System scenario-based program

---
+++ oops-practice/scenario-based/StudentCourseRegistrationSystem/CourseLimitExceededException.java
@@ -0,0 +1,8 @@
+package StudentCourseRegistrationSystem;
+
+public class CourseLimitExceededException extends Exception {
+	public CourseLimitExceededException(String message) {
+		super(message);
+	}
+
+}
