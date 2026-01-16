29fc8399bac6d8b6cd4c512393380141929d605b
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2026-01-16 23:09:30 +0530

    [madhu solanki] Refactor .Added UC-2 with console based contact add in AddressBook

---
+++ oops-practice/gcr-codebase/AddressBook/AddressBookMain.java
@@ -0,0 +1,12 @@
+package AddressBook;
+//main class
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("-------------  Welcome to Address Book Program  -------------");
+
+        AddressBook addressBook = new AddressBook();
+        addressBook.addContact();
+        addressBook.displayContact();
+    }
+}
+
