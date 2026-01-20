c0bb91792cd085ba48d03b923bd81ec6a5ecc980
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2026-01-20 23:42:17 +0530

    [madhu solanki] Add .Added Movie Schedule Manager with Time Validation

---
+++ dsa-practice/scenario-based/CinemaTime/InvalidTimeFormatException.java
@@ -0,0 +1,9 @@
+package CinemaTime;
+import java.util.*;
+
+public class InvalidTimeFormatException extends Exception {
+	//constructor
+	public InvalidTimeFormatException(String msg) {
+		super(msg);
+	}
+}
