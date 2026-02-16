e8460e32fff46840acd29d99e8aeb276e035fe8f
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2026-02-16 12:54:01 +0530

    [madhu solanki] Add .Added Setup for Employee Wage Computation maven project with welcome message

---
+++ java8-features-practice/scenario-based/employee-wage-computation/src/main/java/com/bridgelabz/employeewage/EmployeeWageApplication.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.employeewage;
+import java.util.*;
+
+public class EmployeeWageApplication {
+	public static void main(String[] args) {
+		System.out.println("--------------------------------------------");
+		System.out.println("Welcome to Employee Wage Computation Program");
+		System.out.println("--------------------------------------------");
+	}
+
+}
